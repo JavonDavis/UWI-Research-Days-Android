a9cc0779b6d6d7283c9b969caa45a1ab9517b5fe
Howard <howarde.jr@hotmail.com>
2016-02-13 10:44:41 -0500

    schedule of events now sorted by time

--- app/src/main/java/com/uwics/uwidiscover/utils/ParseController.java
+++ app/src/main/java/com/uwics/uwidiscover/utils/ParseController.java
@@ -7,7 +7,12 @@ import com.parse.ParseObject;
 import com.uwics.uwidiscover.classes.models.Event;
 import com.uwics.uwidiscover.classes.models.Faculty;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -33,16 +38,29 @@ public class ParseController extends Application {
     public void setEventList(List<Event> eventList) {
         this.eventList = eventList;
 
-//        Collections.sort(eventList, new Comparator<Event>() {
-//            DateFormat format = new SimpleDateFormat("h:mm a");
-//            @Override
-//            public int compare(Event lhs, Event rhs) {
-//                try {
-//                    return format.parse(lhs.getStartTime()).compareTo(format.parse(rhs.getStartTime()));
-//                } catch (ParseException e) {
-//                    throw new IllegalArgumentException(e);
-//                }
-//            }
-//        });
+        for (Event e : eventList) {
+            if (e.getStartTime().contains("AM"))
+                e.setStartTime(e.getStartTime().replace("AM", " AM"));
+            else
+                e.setStartTime(e.getStartTime().replace("PM", " PM"));
+
+            if (e.getEndTime().contains("AM"))
+                e.setEndTime(e.getEndTime().replace("AM", " AM"));
+            else
+                e.setEndTime(e.getEndTime().replace("PM", " PM"));
+        }
+
+        Collections.sort(eventList, new Comparator<Event>() {
+            DateFormat format = new SimpleDateFormat("h:mm a");
+
+            @Override
+            public int compare(Event lhs, Event rhs) {
+                try {
+                    return format.parse(lhs.getStartTime()).compareTo(format.parse(rhs.getStartTime()));
+                } catch (ParseException e) {
+                    throw new IllegalArgumentException(e);
+                }
+            }
+        });
     }
 }
