3c094f2188707c1abf4592f07369c7a61f237d6c
Howard <howarde.jr@hotmail.com>
2016-02-12 23:53:16 -0500

    starting meet our sponsors page

--- app/src/main/java/com/uwics/uwidiscover/activities/MeetOurSponsorsActivity.java
+++ app/src/main/java/com/uwics/uwidiscover/activities/MeetOurSponsorsActivity.java
@@ -2,7 +2,9 @@ package com.uwics.uwidiscover.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.uwics.uwidiscover.R;
 
@@ -15,5 +17,19 @@ public class MeetOurSponsorsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activty_meet_our_sponsors);
+
+        if (getActionBar() != null)
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
