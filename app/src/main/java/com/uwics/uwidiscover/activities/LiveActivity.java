6dc4242661ce5e623a4350cdc9e433c9019d4c6e
Howard <howarde.jr@hotmail.com>
2016-02-13 10:27:11 -0500

    fixed livestream link

--- app/src/main/java/com/uwics/uwidiscover/activities/LiveActivity.java
+++ app/src/main/java/com/uwics/uwidiscover/activities/LiveActivity.java
@@ -35,11 +35,13 @@ public class LiveActivity extends AppCompatActivity {
         WebView mWebView = (WebView) findViewById(R.id.webview);
         mWebView.setWebViewClient(new WebClient());
 
-        // mWebView.loadUrl("http://tv.mona.uwi.edu/");
-        mWebView.loadUrl("http://player.twitch.tv/?volume=0.5&channel=esl_sc2&time=14");
+
+//         mWebView.loadUrl("http://tv.mona.uwi.edu/");
+//        mWebView.loadUrl("http://player.twitch.tv/?volume=0.5&channel=esl_sc2&time=14");
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://www.uwicfptv.com/livestream");
     }
 
     @Override
