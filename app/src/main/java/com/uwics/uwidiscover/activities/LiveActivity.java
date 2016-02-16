41d5029be6088089c70234273616494821b804bb
Howard <howarde.jr@hotmail.com>
2016-02-15 22:29:22 -0500

    LiveActivity finished

--- app/src/main/java/com/uwics/uwidiscover/activities/LiveActivity.java
+++ app/src/main/java/com/uwics/uwidiscover/activities/LiveActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
