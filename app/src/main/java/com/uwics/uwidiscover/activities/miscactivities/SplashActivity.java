dbaaca406dd20aef4ef3f31de240d682fc106201
Rajay J. Bitter <rajayjb@gmail.com>
2016-11-20 21:14:20 -0500

    coming soon activity

--- app/src/main/java/com/uwics/uwidiscover/activities/miscactivities/SplashActivity.java
+++ app/src/main/java/com/uwics/uwidiscover/activities/miscactivities/SplashActivity.java
@@ -122,7 +122,8 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                //startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                startActivity(new Intent(SplashActivity.this, TempActivity.class));
                 finish();
             }
         }, secondsDelayed * 1000);
